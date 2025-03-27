16c2cd03c277e825d46bb7bc4e549b4a8ced3fe8
Andre-Sampaio-Filho <andre.claudino04@gmail.com>
2025-03-27 20:12:34 -0300

    correção do link

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ document.getElementById("gerarToken").addEventListener("click", function() {
     const token = Math.random().toString(36).substr(2, 10); // Gera um token aleatório
     localStorage.setItem("avaliacaoToken", token); // Salva o token temporariamente
 
-    const link = `avaliacao.html?token=${token}`;
+    const link = `index2.html?token=${token}`;
     document.getElementById("linkGerado").innerHTML = `<a href="${link}" target="_blank">${link}</a>`;
 });
